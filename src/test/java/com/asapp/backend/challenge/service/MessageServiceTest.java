d91ac6bd7b2835ff88e431981345bdf8d2ba1b61
rsantaeulalia <rodrigo.santaeulalia@gmail.com>
2021-07-05 16:53:25 -0300

    refactor test classes

--- src/test/java/com/asapp/backend/challenge/service/MessageServiceTest.java
+++ src/test/java/com/asapp/backend/challenge/service/MessageServiceTest.java
@@ -1,25 +1,5 @@
 package com.asapp.backend.challenge.service;
 
-import com.asapp.backend.challenge.model.Message;
-import com.asapp.backend.challenge.repository.MessageRepository;
+public class MessageServiceTest {
 
-import java.util.Collection;
-
-public class MessageServiceTest implements MessageService {
-
-    private final MessageRepository messageRepository;
-
-    public MessageServiceTest(MessageRepository messageRepository) {
-        this.messageRepository = messageRepository;
-    }
-
-    @Override
-    public Message saveMessage(Message message) {
-        return messageRepository.addMessage(message);
-    }
-
-    @Override
-    public Collection<Message> getMessagesByReceiverId(Long receiverId, Long startId, Long limit) {
-        return messageRepository.getMessages(receiverId, startId, limit);
-    }
 }
