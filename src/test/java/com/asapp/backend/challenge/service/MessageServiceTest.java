7bf0c85e9a8fa870e2dfc45abd729984df6897f9
rsantaeulalia <rodrigo.santaeulalia@gmail.com>
2021-07-05 17:59:36 -0300

    added services test

--- src/test/java/com/asapp/backend/challenge/service/MessageServiceTest.java
+++ src/test/java/com/asapp/backend/challenge/service/MessageServiceTest.java
@@ -1,5 +1,63 @@
 package com.asapp.backend.challenge.service;
 
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+
+import com.asapp.backend.challenge.model.Image;
+import com.asapp.backend.challenge.model.Message;
+import com.asapp.backend.challenge.model.Video;
+import com.asapp.backend.challenge.model.enums.SourceEnum;
+import com.asapp.backend.challenge.repository.MessageRepository;
+import com.asapp.backend.challenge.service.implementation.MessageServiceImpl;
+import com.sun.tools.javac.util.List;
+import org.easymock.EasyMock;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.Date;
+
 public class MessageServiceTest {
+
+    private final Message message = new Message(2L, 1L, new Video("video", "https://videourl.com", SourceEnum.YOUTUBE), new Date());
+    private final Message expectedMessage = new Message(1L, 2L, 1L, new Video("video", "https://videourl.com", SourceEnum.YOUTUBE), new Date());
+    private final Message expectedMessage2 = new Message(2L, 2L, 1L, new Image("image", "https://videourl.com", 100, 200), new Date());
+
+    private MessageService messageService;
+    private MessageRepository messageRepository;
+
+    @Before
+    public void setUp() {
+        messageRepository = EasyMock.createMock(MessageRepository.class);
+        messageService = new MessageServiceImpl(messageRepository);
+    }
+
+    @Test
+    public void givenAMessageWhenServiceSaveMethodIsCalledThenReturnMessage() {
+        expect(messageRepository.addMessage(message)).andReturn(expectedMessage);
+
+        replay(messageRepository);
+
+        Message savedMessage = messageService.saveMessage(message);
+
+        Assert.assertEquals(savedMessage.getId(), expectedMessage.getId());
+
+        verify(messageRepository);
+    }
+
+    @Test
+    public void givenQueryParamsWhenGetMessagesIsCalledThenReturnCollectionOfMessages() {
+        expect(messageRepository.getMessages(1L, 1L, 2L)).andReturn(List.of(expectedMessage, expectedMessage2));
+
+        replay(messageRepository);
+
+        Collection<Message> savedMessage = messageService.getMessagesByReceiverId(1L, 1L, 2L);
+
+        Assert.assertEquals(savedMessage.size(), 2);
+
+        verify(messageRepository);
+    }
 
 }
--- src/test/java/com/asapp/backend/challenge/service/UserServiceTest.java
+++ src/test/java/com/asapp/backend/challenge/service/UserServiceTest.java
@@ -1,5 +1,72 @@
 package com.asapp.backend.challenge.service;
 
-public class UserServiceTest {
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
 
+import com.asapp.backend.challenge.model.User;
+import com.asapp.backend.challenge.repository.UserRepository;
+import com.asapp.backend.challenge.service.implementation.UserServiceImpl;
+import org.easymock.EasyMock;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Date;
+import java.util.Optional;
+
+public class UserServiceTest {
+    private final User user = new User("Testusername", "hashedPassword", new Date());
+    private final User expectedUser = new User(1L, "Testusername", "hashedPassword", new Date());
+
+    private UserService userService;
+    private UserRepository userRepository;
+
+    @Before
+    public void setUp() {
+        userRepository = EasyMock.createMock(UserRepository.class);
+        userService = new UserServiceImpl(userRepository);
+    }
+
+    @Test
+    public void givenAUserWhenServiceSaveMethodIsCalledThenReturnUser() {
+        expect(userRepository.addUser(user)).andReturn(expectedUser);
+
+        replay(userRepository);
+
+        User savedUser = userService.registerUser(user);
+
+        Assert.assertEquals(savedUser.getId(), expectedUser.getId());
+
+        verify(userRepository);
+    }
+
+    @Test
+    public void givenUsernameWhenGetByUsernameIsCalledThenReturnUser() {
+        expect(userRepository.getByUsername("Testusername")).andReturn(Optional.of(expectedUser));
+
+        replay(userRepository);
+
+        Optional<User> savedUser = userService.getUserByUsername("Testusername");
+
+        Assert.assertTrue(savedUser.isPresent());
+        Assert.assertEquals(savedUser.get().getUsername(), expectedUser.getUsername());
+        Assert.assertEquals(savedUser.get().getPassword(), expectedUser.getPassword());
+
+
+        verify(userRepository);
+    }
+
+    @Test
+    public void givenWrongUsernameWhenGetByUsernameIsCalledThenReturnEmpty() {
+        expect(userRepository.getByUsername("wrongtestusername")).andReturn(Optional.empty());
+
+        replay(userRepository);
+
+        Optional<User> savedUser = userService.getUserByUsername("wrongtestusername");
+
+        Assert.assertTrue(savedUser.isEmpty());
+
+        verify(userRepository);
+    }
 }
